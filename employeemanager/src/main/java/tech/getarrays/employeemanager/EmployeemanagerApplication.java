e7b27ef36b06955305420e1f37fcddeed99977d0
AppleMac0315 <109144464+Applemac0315@users.noreply.github.com>
2022-09-06 20:53:46 +0400

    update

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
